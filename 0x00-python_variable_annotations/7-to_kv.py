a0cce660e3cb6fb381e805fadbc2cd3c56117f93
jassemb <benalijassem201@gmail.com>
2022-06-07 17:14:52 +0100

    str square

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from typing import List, Union
+
+
+def to_kv(k: str, v: [int, float]) -> Tuple[str, float]:
+    """
+    returns a tuple
+    """
+    return (k, v**2)
