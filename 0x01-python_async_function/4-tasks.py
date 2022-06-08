28b67fc1aff52ddced6cb6e5a89a51cfeecec53a
jassemb <benalijassem201@gmail.com>
2022-06-08 20:40:47 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,16 +4,14 @@ module for the wait
 """
 import asyncio
 from typing import List
-
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
+
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    same as identical to wait_n
-    """
-    list_random = []
-    for _ in range(n):
-        list_random.append(await task_wait_random(max_delay))
-    return sorted(list_random)
+    """return the list of all the delays (float values)"""
+    delay_list: List[float] = []
+    for i in range(n):
+        delay_list.append(await task_wait_random(max_delay))
+    return sorted(delay_list)
