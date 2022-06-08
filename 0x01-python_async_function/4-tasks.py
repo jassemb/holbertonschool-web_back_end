af925d9866ad4d31185f1b19ee35917d6f65e6cf
jassemb <benalijassem201@gmail.com>
2022-06-08 20:42:11 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-module for the wait
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is
+being called.
 """
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
-
-
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
