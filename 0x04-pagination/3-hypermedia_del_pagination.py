656acbb41343290e7b17db1867357987bafe9f9b
jassemb <benalijassem201@gmail.com>
2022-06-14 16:37:08 +0100

    Deletion-resilient hypermedia pagination

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -14,6 +14,7 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -25,6 +26,20 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        pass
 
     def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
