1771e2f7cc1d1e0e667ac779d06e8f30641cb817
jassemb <benalijassem201@gmail.com>
2022-06-17 09:45:08 +0100

    filtered_logger.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-"""
-log message obfuscated
-"""
 import re
 from typing import List
 import logging
+import mysql.connector
+from mysql.connector import Error
 import os
 
 
@@ -12,25 +10,27 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
+    """ Redacting Formatter class
+        """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ constructor """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ generates a log"""
         msg = logging.Formatter(self.FORMAT).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """
-    A function that returns the log message obfuscated
-    """
+    """ A function that returns the log message obfuscated """
     lst = message.split(separator)
 
     for f in fields:
