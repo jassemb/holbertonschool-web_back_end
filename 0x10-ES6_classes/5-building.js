e45abe8a9dedbd8cef8dabb492fdeedb017426ec
jassemb <benalijassem201@gmail.com>
2022-08-09 10:36:32 +0100

    building

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,9 +1,9 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
-        if (!this.evacuationWarningMessage) { throw TypeError('Class extending Building must override evacuationWarningMessage'); }
-      }
-      if (typeof (sqft) !== 'number') throw TypeError('must be a number');
+      if (!this.evacuationWarningMessage) { throw TypeError('Class extending Building must override evacuationWarningMessage'); }
+    }
+    if (typeof (sqft) !== 'number') throw TypeError('must be a number');
     this._sqft = sqft;
   }
 
