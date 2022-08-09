c531a70d89bfed5e25f74ac94c020bb9bb643fba
jassemb <benalijassem201@gmail.com>
2022-08-09 09:34:46 +0100

    2-hbtn_course

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,9 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== 'string') throw TypeError('name must be a srting');
     if (typeof (length) !== 'number') throw TypeError('length must be a number');
-    if (!Array.isArray(students)) throw TypeError('students must be an array');
+    if (students.every((i) => (typeof i !== 'string'))) {
+      throw Error('Students must be an Array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -31,7 +33,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!Array.isArray(students)) throw TypeError('Students must be an array');
+    if (!Array.isArray(students)) throw TypeError('Students must be an Array of strings');
     this._students = students;
   }
 }
