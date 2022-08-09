bdf6c874e46ecb37a9c6fb7c3584ba585270af35
jassemb <benalijassem201@gmail.com>
2022-08-09 14:03:29 +0100

    promise 1

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,8 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise(() => { if (success) {
+    return new Promise((resolve, reject) => { 
+        if (success) {
         resolve({ status: 200, body: 'Success' });
       } else {
-        reject(Error('The fake API is not working currently'));
-      }});
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
   }
   
