b64eaa05892a27d554d26a90c421903c27032e52
jassemb <benalijassem201@gmail.com>
2022-08-09 09:30:50 +0100

    2-hbtn_course

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -31,7 +31,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!Array.isArray(students)) throw TypeError('students must be an array');
+    if (!Array.isArray(students)) throw TypeError('Students must be an array');
     this._students = students;
   }
 }
