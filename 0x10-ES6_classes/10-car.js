0449ae4fbf61f93035e0ec0b40638095cf97de5d
jassemb <benalijassem201@gmail.com>
2022-08-09 12:12:09 +0100

    clone car

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class Car {
+    constructor(brand,motor,color) {
+        this._brande = brand;
+        this._motor = motor;
+        this._color = color;
+      }
+      static get [Symbol.species]() {
+        return this;
+      }
+      cloneCar() {
+        const NewObj = this.constructor[Symbol.species];
+        return new NewObj(this._brand, this._motor, this._color);
+      }
+}
