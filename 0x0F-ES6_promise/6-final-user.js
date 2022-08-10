c47d8128b9b4ede700cd2aabfac1a7a339dfc8c5
jassemb <benalijassem201@gmail.com>
2022-08-10 12:26:55 +0100

    final usr

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName, lastName, filename) {
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((data) => {
-        for (const res of data) {
-          if (res.status === 'rejected') {
-            res.value = res.reason.message;
-            return res.reason;
-          }
+      for (const res of data) {
+        if (res.status === 'rejected') {
+          res.value = res.reason.message;
+          return res.reason;
         }
-        return data;
-      })
+      }
+      return data;
+    });
 }
