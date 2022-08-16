7b74bdd40d9f705be525f7055e7f47aa0cc129f3
jassemb <benalijassem201@gmail.com>
2022-08-16 14:45:13 +0100

    clalble option data

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,22 +3,29 @@
 Redis basic
 """
 import redis
-from typing import Union
+from typing import Optional, Union, Callable
 import uuid
+
 
 class Cache():
     """Cache class"""
+
     def __init__(self):
         """Intizializer"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-            """
-            method should generate a random key
-            Return:
-                Random generated key
-            """
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
-            return key
+        """
+        method should generate a random key
+        Return:
+            Random generated key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        return data
