677aece75d4e9a80c6cae45c04fad9c88ce302fd
jassemb <benalijassem201@gmail.com>
2022-08-22 09:34:33 +0100

    uniq_users

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
+-- SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `email` varchar(255), NOT NULL UNIQUE,
-    `name` varchar(255),
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
     PRIMARY KEY (id)
 );
