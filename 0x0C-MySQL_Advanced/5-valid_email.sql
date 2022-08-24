9fc703385f8fbddccef1e85df089dd1d8aab7c08
jassemb <benalijassem201@gmail.com>
2022-08-24 09:26:41 +0100

    valid email

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@
 DELIMITER //
 CREATE TRIGGER `valid_email_trigger` AFTER UPDATE ON `users` FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END //
