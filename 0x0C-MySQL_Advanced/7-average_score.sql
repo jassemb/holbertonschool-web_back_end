f3c317409c65c901eb0379d8cc3469199ed96667
jassemb <benalijassem201@gmail.com>
2022-08-24 09:47:47 +0100

    average_score

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,7 @@
--- a SQL script
--- that creates an index idx_name_first on the table
--- names and the first letter of name.
+-- a SQL script a SQL script that creates
+-- a stored procedure ComputeAverageScoreForUser that computes and store the average score
+-- for a student. 
+-- Note: An average score can be a decimal
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
     BEGIN
