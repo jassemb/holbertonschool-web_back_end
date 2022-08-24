25d3474b5745294d0b01adb6cd646bd38997a10f
jassemb <benalijassem201@gmail.com>
2022-08-24 11:04:20 +0100

    need meeting

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 -- have a score under 80 (strict) and no last_meeting or more than 1 month.
 CREATE VIEW need_meeting
 AS SELECT name FROM students WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting > DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
