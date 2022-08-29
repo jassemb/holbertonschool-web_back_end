7e954f67d81c26a8cd0ca9ad85407ee7071927a4
jassemb <benalijassem201@gmail.com>
2022-08-29 14:52:25 +0100

    get students

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(arrayobjects, city) {
-    if (!(Array.isArray(arrayobjects))) {
-        return none;
-      }
-        return arrayobjects.filter((value) => value.location === city);
+  if (!(Array.isArray(arrayobjects))) {
+    return null;
+  }
+  return arrayobjects.filter((value) => value.location === city);
 }
