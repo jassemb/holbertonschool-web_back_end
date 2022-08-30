a13af3c4f6b2defef1b6098e2b948bf729fed9f7
jassemb <benalijassem201@gmail.com>
2022-08-30 16:06:34 +0100

    clean_set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet (set,startString) {
-    if (startString.lenvigth === 0) {
-        return '';
-      }
-      const arr = [];
-      set.forEach((element) => {
-        if (element.startsWith(startString)) {
-          arr.push(element.replace(startString, ''));
-        }
-      });
-    return ( arr.join("-"));
+  if(startString?.length === 0 ) {
+    return "";
+  }
+  const arr = [];
+  set.forEach((element) => {
+    if (element?.startsWith(startString)) {
+      arr.push(element.replace(startString, ''));
+    }
+  });
+    return ( arr.join("_"));
 }
