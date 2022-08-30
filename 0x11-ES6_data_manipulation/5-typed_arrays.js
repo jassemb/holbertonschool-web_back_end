0043c422e56d3bb8e04fc9331a75a8f36e2ec73f
jassemb <benalijassem201@gmail.com>
2022-08-30 13:35:56 +0100

    5-typed_arrays

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
-export default function createInt8TypedArray(length,position,value) {
-    
+export default function createInt8TypedArray(length, position, value) {
+
     try {
-    const typedArray = new Int8Array(length);
-    typedArray[position] = value;
-    return typedArray;
-      } catch (e) {
+        const buffer = new ArrayBuffer(length);
+        const dataView = new DataView(buffer);
+        dataView.setInt8(position, value);
+        return dataView;
+    } catch (e) {
         throw Error('Position outside range');
-      }
+    }
 }
