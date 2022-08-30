07b21bbda1417dd0a322d99d8750e18e234e496a
jassemb <benalijassem201@gmail.com>
2022-08-30 16:04:57 +0100

    clean_set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,10 @@
 export default function cleanSet (set,startString) {
-    /* eslint-disable */
-    if (startString?.lenvigth === 0) {
+    if (startString.lenvigth === 0) {
         return '';
       }
       const arr = [];
       set.forEach((element) => {
-        if (element?.startsWith(startString)) {
+        if (element.startsWith(startString)) {
           arr.push(element.replace(startString, ''));
         }
       });
