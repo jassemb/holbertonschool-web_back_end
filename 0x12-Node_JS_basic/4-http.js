b3585a226b4770de388428b3cc3c66b946a365b2
jassemb <benalijassem201@gmail.com>
2022-09-02 09:45:35 +0100

    create server

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,16 +1,15 @@
-var http = require('http');
+const http = require('http');
 
-
-const app = http.createServer(function (req,res) {
-  res.write('Hello Holberton School!'); 
-  res.end(); 
+const app = http.createServer((res) => {
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(1245, (error) => {
-    if (error) {
-      console.log('Something went wrong', error);
-    } else {
-      console.log(`Server is listening on port 1245`);
-    }
-  });
-  module.exports = app;
+  if (error) {
+    console.log('Something went wrong', error);
+  } else {
+    console.log('Server is listening on port 1245');
+  }
+});
+module.exports = app;
