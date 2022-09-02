113905c6db7ced98a31cc9c30f7d7d4ac1a33658
jassemb <benalijassem201@gmail.com>
2022-09-02 14:15:43 +0100

    http_express

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -9,21 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
-    countStudents(database)
-      .then((data) => {
-        const { students, csStudents, sweStudents } = data;
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${students.length}\n`);
-        res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
-        res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-        res.statusCode = 200;
-        res.end();
-      })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.send(`This is the list of our students\n${error.message}`);
-      });
-  });
+  countStudents(database)
+    .then((data) => {
+      const { students, csStudents, sweStudents } = data;
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.statusCode = 200;
+      res.end();
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
 
 app.listen(port, () => {
 
