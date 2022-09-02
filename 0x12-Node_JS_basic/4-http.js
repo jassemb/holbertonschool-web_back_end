c78b32522823672ab329ecda19deafeeadc58b37
jassemb <benalijassem201@gmail.com>
2022-09-02 09:56:31 +0100

    http

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 const http = require('http');
+
 const port = 1245;
-const app = http.createServer((req,res) => {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
   res.end();
 });
