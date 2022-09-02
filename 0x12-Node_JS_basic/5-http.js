b1f62e596a4d500fa43d623c88c70c056e83c3a8
jassemb <benalijassem201@gmail.com>
2022-09-02 12:16:56 +0100

    http

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,4 +1,3 @@
-const { debug } = require('console');
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
@@ -14,22 +13,20 @@ const app = http.createServer(async (req, res) => {
     } else if (req.url === '/students') {
         res.write('This is the list of our students\n');
         try {
-            var { data, csStudents, sweStudents } = await countStudents(DATABASE);
-
-            res.write(`Number of students: ${data.length}\n`);
+            const { students, csStudents, sweStudents } = await countStudents(DATABASE);
+            res.write(`Number of students: ${students.length}\n`);
             res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
             res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
             res.end();
-        } catch (error) {
+          } catch (error) {
             res.end(error.message);
-        }
-    } else {
-        res.writeHead(404);
-        res.end('Invalid request');
-      }
+          }
+        } else {
+          res.writeHead(404);
+          res.end('Invalid request');
+        } 
+      
 });
 
-app.listen(PORT);
+app.listen(PORT,HOSTNAME);
 module.exports = app;
-
-
