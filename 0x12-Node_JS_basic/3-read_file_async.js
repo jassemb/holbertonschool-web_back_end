d15a79f46d78526189a13a38b03fdba92d4fc982
jassemb <benalijassem201@gmail.com>
2022-09-02 10:10:38 +0100

    async file

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 
 async function countStudents(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, (error, dataFile) => {
-          if (error) {
-            reject(Error('Cannot load the database'));
-          }
-          if (dataFile) {
-            const fields = {};
-            let data = dataFile.toString().split('\n');
-            data = data.filter((elem) => elem.length > 0);
-            data.shift();
-    
-            data.forEach((elem) => {
-              if (elem.length > 0) {
-                const value = elem.split(',');
-                if (value[3] in fields) {
-                  fields[value[3]].push(value[0]);
-                } else {
-                  fields[value[3]] = [value[0]];
-                }
-              }
-            });
-            console.log(`Number of students: ${data.length}`);
-            for (const field in fields) {
-              if (field) {
-                const res = fields[field];
-                console.log(`Number of students in ${field}: ${res.length}. List: ${res.join(', ')}`);
-              }
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, dataFile) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      }
+      if (dataFile) {
+        const fields = {};
+        let data = dataFile.toString().split('\n');
+        data = data.filter((elem) => elem.length > 0);
+        data.shift();
+
+        data.forEach((elem) => {
+          if (elem.length > 0) {
+            const value = elem.split(',');
+            if (value[3] in fields) {
+              fields[value[3]].push(value[0]);
+            } else {
+              fields[value[3]] = [value[0]];
             }
           }
-          resolve();
         });
-      });
+        console.log(`Number of students: ${data.length}`);
+        for (const field in fields) {
+          if (field) {
+            const res = fields[field];
+            console.log(`Number of students in ${field}: ${res.length}. List: ${res.join(', ')}`);
+          }
+        }
+      }
+      resolve();
+    });
+  });
 }
 module.exports = countStudents;
